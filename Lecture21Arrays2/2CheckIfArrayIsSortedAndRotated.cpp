e998c04c175b72ebf2e8d1fb195994ef335dbd4e
anushkakawale <anushkakawale111@gmail.com>
2025-10-22 23:20:53 +0530

    Lecture22Updated

--- Lecture21Arrays2/2CheckIfArrayIsSortedAndRotated.cpp
+++ Lecture21Arrays2/2CheckIfArrayIsSortedAndRotated.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main(){
     vector<int> nums = {3,4,5,1,2};
-    int count = 0, n = nums.size();
-    for(int i=1; i<n; i++){
-        if(nums[i] < nums[i-1]){
+    int count = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
             count++;
         }
     }
-    if(nums[n-1] > nums[0]){
-        count++;
-    }
 
-    if(count==1){
+    if(count<=1){
         cout<<"Array is Sorted and Rotated"<<endl;
     }
     else{
